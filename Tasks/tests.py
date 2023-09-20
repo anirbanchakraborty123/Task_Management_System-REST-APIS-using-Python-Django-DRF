44a0a1cabb340fd404902b2a194bdd8a729b50cb
anirbanchakraborty123 <anirbanchakraborty714@gmail.com>
2023-09-20 17:21:25 +0530

    added all test cases

--- Tasks/tests.py
+++ Tasks/tests.py
@@ -1,23 +1,75 @@
 from django.test import TestCase
 from rest_framework.test import APIClient
 from .models import Task
+from rest_framework_simplejwt.tokens import AccessToken
+from django.contrib.auth.models import User
 
 class TaskAPITestCase(TestCase):
     def setUp(self):
+        user = User.objects.create_user(
+                username='test', email='test@gmail.com', password='top_secret')
         self.client = APIClient()
-        self.task1 = Task.objects.create(title="Task 1", description="Description 1", status="in_progress", due_date="2023-09-30")
-        self.task2 = Task.objects.create(title="Task 2", description="Description 2", status="completed", due_date="2023-10-15")
+        token = AccessToken.for_user(user=user)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {token}')
+        self.task_data = {
+            'title': 'Test Task',
+            'description': 'This is a test task.',
+            'status': 'created',
+            'due_date': '2023-09-28',
+        }
+        self.task = Task.objects.create(**self.task_data)
+        self.task1 = Task.objects.create(title="Task 1", description="Description 1", status="in_progress", due_date="2023-09-20")
+        self.task2 = Task.objects.create(title="Task 2", description="Description 2", status="completed", due_date="2023-10-25")
+        
+    def test_create_task(self):
+        """ Test case to check create new task - api """
+        
+        response = self.client.post('/api/v1/tasks/', self.task_data, format='json')
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(Task.objects.count(), 4)
+        
+    def test_retrieve_task(self):
+        """ Test case to check task with specific id - api"""
+        
+        response = self.client.get(f'/api/v1/tasks/{self.task.id}/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['title'], self.task_data['title'])
 
+    def test_update_task(self):
+        """ Test case to check update task for particular id  - api"""
+        updated_data = {
+            'title': 'Updated Task',
+            'status': 'completed',
+        }
+        response = self.client.patch(f'/api/v1/tasks/{self.task.id}/', updated_data, format='json')
+        self.assertEqual(response.status_code, 200)
+        self.task.refresh_from_db()
+        self.assertEqual(self.task.title, updated_data['title'])
+        self.assertEqual(self.task.status, updated_data['status'])
+
+    def test_delete_task(self):
+        """ Test case to delete task with specific id - api"""
+        
+        response = self.client.delete(f'/api/v1/tasks/{self.task.id}/')
+        self.assertEqual(response.status_code, 204)
+        self.assertEqual(Task.objects.filter(id=self.task.id).count(), 0)  # Check that the task is deleted
+    
     def test_sort_by_due_date(self):
+        """ Test case to test the Task sort - api. """
+        
         response = self.client.get('/api/v1/tasks/sort/?sort_by=due_date')
         self.assertEqual(response.status_code, 200)
         sorted_tasks = response.json()
-        self.assertEqual(sorted_tasks['count'], 2)
-        
+        self.assertEqual(sorted_tasks['count'], 3) # Checks Valid Response
+        self.assertNotEqual(sorted_tasks['count'], 8) # Checks Invalid/Incorrect Response
+       
     def test_filter_by_due_date(self):
-        response = self.client.get('/api/v1/tasks/filter/?due_date=2023-09-30')
+        """ Test case to test the Task filter - api. """
+        
+        response = self.client.get('/api/v1/tasks/filter/?due_date=2023-09-20')
         self.assertEqual(response.status_code, 200)
         sorted_tasks = response.json()
-        self.assertEqual(sorted_tasks['count'], 1)
+        self.assertEqual(sorted_tasks['count'], 1) # Checks Valid Response
+        self.assertNotEqual(sorted_tasks['count'], 8) # Checks Invalid/Incorrect Response
         
 
--- readme.md
+++ readme.md
@@ -6,68 +6,82 @@ This is a RESTful API for managing tasks with features like pagination, filterin
 
 1. Clone the repository:
 
-git clone <repository_url>
+   - git clone https://github.com/anirbanchakraborty123/Task_Management_System-REST-APIS-using-Python-Django-DRF.git
 
 2. Create a virtual environment:
 
-python -m venv venv
+   - python -m venv venv
 
 3. Activate the virtual environment:
-- On Windows:
+   - On Windows:
+       venv\Scripts\activate
   
-  venv\Scripts\activate
+   - On macOS and Linux:
+       source venv/bin/activate
   
-- On macOS and Linux:
-  
-  source venv/bin/activate
-  
-
 4. Install dependencies:
 
-pip install -r requirements.txt
+   - pip install -r requirements.txt
 
 5. Apply migrations:
 
-python manage.py migrate
+   - python manage.py migrate
 
 6. Start the development server:
 
-python manage.py runserver
+   - python manage.py runserver
 
-7. The API will be accessible at 'http://localhost:8000/'.
+7. The API's will be accessible at 'http://localhost:8000/api/v1/' with mandatory JWT bearer access_token Authentication Header.
+
+   - You can get new access_token using login creds-(username,passsowrd) on below apis
+   - Get new access_token 'POST /token/'
+      - payload={
+        username,password
+      }
+      - response={
+        access_token,refresh_token
+      }
+   - Refresh expired access_token by passing refresh_token to 'POST /token/refresh/'
+     - payload={
+        refresh_token
+     }
+     - response={
+        access_token,refresh_token
+     }
+    
+### Swagger OPEN API Documentation:
+
+   You can view swagger api document of all available Rest apis:
+   - You can test them after authentication:
+   - http://127.0.0.1:8000/docs/
 
 ## API Endpoints
 
-- Create a new task: 'POST /tasks/'
-- Retrieve a specific task by ID: 'GET /tasks/<task_id>/'
-- Retrieve all tasks: 'GET /tasks/'
-- Update a task: 'PUT /tasks/<task_id>/'
-- Delete a task: 'DELETE /tasks/<task_id>/'
-- Sort tasks: 'GET /tasks/sort/?sort_by=due_date or ?sort_by=title'
-- Filter tasks: 'GET /tasks/filter/?status=in_progress or ?due_date=due_date'
+   - Create a new task: 'POST /tasks/'
+   - Retrieve a specific task by ID: 'GET /tasks/<task_id>/'
+   - Retrieve all tasks: 'GET /tasks/'
+   ![alt text](https://ibb.co/jJS3Nv6)
+   - Update a task: 'PUT /tasks/<task_id>/'
+   - Delete a task: 'DELETE /tasks/<task_id>/'
+   - Sort tasks: 'GET /tasks/sort/?sort_by=due_date or ?sort_by=title'
+   - Filter tasks: 'GET /tasks/filter/?status=in_progress or ?due_date=due_date'
 
 ### Filtering and Sorting
 
-You can filter and sort tasks by adding query parameters to the endpoint:
-- Filter by status: '?status=<status>'
-- Filter by due date: '?due_date=<date>'
-- Sort by due date: '?sort_by=due_date'
-- Sort by title: '?sort_by=title'
+   You can filter and sort tasks by adding query parameters to the endpoint:
+   - Filter by status: '?status=<status>'
+   - Filter by due date: '?due_date=<date>'
+   - Sort by due date: '?sort_by=due_date'
+   - Sort by title: '?sort_by=title'
 
-### Swagger OPEN API Documentation:
-
-You can view swagger api document of all available Rest apis:
-- You can test them after authentication:
-- http://127.0.0.1:8000/docs/
 
 8. Running Tests
 
-To run unit tests, use the following command:
-
-python manage.py test
+   - To run unit tests, use the following command:
+    python manage.py test
 
 9. Run the Development Server
 
-python manage.py runserver
+   python manage.py runserver
 
 
